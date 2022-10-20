878232204a647ed38e70bf9a50b9156a50ef49e3
CalciumG <52017802+CalciumG@users.noreply.github.com>
2022-10-20 10:29:32 +0100

    add small shadow and alt

--- components/ListWithImage.tsx
+++ components/ListWithImage.tsx
@@ -14,11 +14,17 @@ export const ListWithImage: React.FC<TopListProps> = ({ title, data }) => {
         {data &&
           data?.map((item) => (
             <li
-              className="flex w-[300px] items-center py-2 text-lg child:pl-2 sm:max-w-xs"
+              className="flex w-[300px] items-center py-2 text-lg sm:max-w-xs"
               key={item.name}
             >
-              <img alt="ally" src={item.imageUrl} height={75} width={75} />
-              <p className="line-clamp-2">{item.name}</p>
+              <img
+                alt={item.name}
+                src={item.imageUrl}
+                height={70}
+                width={70}
+                className="shadow-xl"
+              />
+              <p className="pl-2 line-clamp-2">{item.name}</p>
             </li>
           ))}
       </ol>
