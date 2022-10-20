6705a7ed54ac31a8a3787112e552fba135e193e6
CalciumG <52017802+CalciumG@users.noreply.github.com>
2022-10-20 22:57:47 +0100

    basic style for a basic error page

--- pages/error.tsx
+++ pages/error.tsx
@@ -1,7 +1,10 @@
 const Error = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-painful-blue text-3xl text-horrid-green">
-      Your email isn't whitelisted. Please convince spotify to extend my quotas.
+    <div className="flex h-screen w-screen items-center justify-center bg-painful-blue text-center text-3xl font-bold text-horrid-green">
+      <p>
+        Your email isn't whitelisted. Please convince spotify to extend my
+        quotas.
+      </p>
     </div>
   );
 };
